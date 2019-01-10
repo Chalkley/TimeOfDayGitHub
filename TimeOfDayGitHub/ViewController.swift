a64f0190da1e5af7f646f5006e383a1edb44dbbd
Jason Chalkley <jaychalks@gmail.com>
2019-01-10 12:35:32 +0000

    Initial Commit

---
+++ TimeOfDayGitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeOfDayGitHub
+//
+//  Created by Jason Chalkley on 10/01/2019.
+//  Copyright © 2019 Jason Chalkley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
